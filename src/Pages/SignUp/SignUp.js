230ed97ce79482703c93db8e807e7869c1baa169
Talimul212 <islamtalimul0@gmail.com>
2022-12-19 18:45:34 +0600

    modifie sign up

--- src/Pages/SignUp/SignUp.js
+++ src/Pages/SignUp/SignUp.js
@@ -4,7 +4,7 @@ import toast from 'react-hot-toast';
 import { Link, useNavigate } from 'react-router-dom';
 import { AuthContext } from '../../Context/AuthProvider/AuthProvider';
 import useToken from '../../hooks/useToken';
-
+import PrimaryButten from '../../Components/PrimaryButten/PrimaryButten';
 
 const SignUp = () => {
     const { register, handleSubmit, formState: { errors } } = useForm();
@@ -17,7 +17,7 @@ const SignUp = () => {
     if (token) {
         navigate('/');
     }
-    
+
     const handleSignUp = (data) => {
         setSignUpError('');
         createUser(data.email, data.password)
@@ -39,8 +39,8 @@ const SignUp = () => {
             })
 
         const saveUser = (name, email) => {
-            const role='seller'
-            const user = { name, email,role};
+            const role = 'seller'
+            const user = { name, email, role };
             fetch('https://tech-com-server.vercel.app/users', {
                 method: 'POST',
                 headers: {
@@ -54,61 +54,57 @@ const SignUp = () => {
                 })
         }
 
-        // const saveUser = (name, email) => {
-        //     const user = { name, email };
-        //     fetch('https://tech-com-server.vercel.app/users', {
-        //         method: 'POST',
-        //         headers: {
-        //             'content-type': 'application/json'
-        //         },
-        //         body: JSON.stringify(user)
-        //     })
-        //         .then(res => res.json())
-        //         .then(data => {
-        //             getUserToken(email);          
-        //         })
-        // }
-
     }
 
     return (
-        <div className='h-[800px] flex justify-center items-center'>
-            <div className='w-96 p-7 shadow-lg rounded-xl'>
-                <h2 className='text-2xl font-semibold text-center mb-6'>Sign Up</h2>
-                <form onSubmit={handleSubmit(handleSignUp)} >
-                    <div className="form-control w-full max-w-xs">
-                        <label className="label"> <span className="label-text font-bold">Name</span></label>
-                        <input type="text" {...register("name", {
-                            required: "Name is Required"
-                        })} className="input input-bordered w-full max-w-xs" />
-                        {errors.name && <p className='text-red-500'>{errors.name.message}</p>}
+        <div className=' flex justify-center items-center'>
+            <div className="hero rounded-lg mt-10" id='heroImg'>
+                <div className="hero-content flex-col lg:flex-row-reverse ">
+                    <div className='w-full p-7 shadow-lg rounded-xl bg-white'>
+                        <h2 className='text-2xl font-semibold text-center mb-6'>Sign Up</h2>
+                        <form onSubmit={handleSubmit(handleSignUp)} >
+                            <div className="form-control w-full ">
+                                <label className="label"> <span className="label-text font-bold">Name</span></label>
+                                <input type="text" {...register("name", {
+                                    required: "Name is Required"
+                                })} className="input input-bordered w-full" />
+                                {errors.name && <p className='text-red-500'>{errors.name.message}</p>}
+                            </div>
+                            <div className="form-control w-full ">
+                                <label className="label"><span className="label-text font-bold">Email</span> </label>
+                                <input type='text' name='email' {...register('email', { required: "email is required" })}
+                                    className="input input-bordered w-full" />
+                                {errors.email && <p className='text-red-600'>{errors.email.message}</p>}
+                            </div>
+                            <div className="form-control w-full ">
+                                <label className="label"><span className="label-text font-bold">Password</span> </label>
+                                <input type='password' name='password' {...register('password'
+                                    , {
+                                        required: "password is required",
+                                        minLength: { value: 6, message: "password must be 6 characters or longer" }
+                                    }
+                                )}
+                                    className="input input-bordered w-full" />
+                                {errors.password && <p className='text-red-600'>{errors.password.message}</p>}
+
+                            </div>
+                            <input type="submit" className=' btn btn-primary  bg-gradient-to-r from-primary  to-secondary my-2 text-white w-full' value='Sign Up as a seller' />
+                            <div>
+                                {singUpError && <p className='text-red-600'>{singUpError}</p>}
+                            </div>
+                        </form>
+                        <p className='font-bold text-center'>Already have an <Link to='/login' className='text-secondary'> account</Link></p>
+
                     </div>
-                    <div className="form-control w-full ">
-                        <label className="label"><span className="label-text font-bold">Email</span> </label>
-                        <input type='text' name='email' {...register('email', { required: "email is required" })}
-                            className="input input-bordered w-full" />
-                        {errors.email && <p className='text-red-600'>{errors.email.message}</p>}
-                    </div>
-                    <div className="form-control w-full ">
-                        <label className="label"><span className="label-text font-bold">Password</span> </label>
-                        <input type='password' name='password' {...register('password'
-                            , {
-                                required: "password is required",
-                                minLength: { value: 6, message: "password must be 6 characters or longer" }
-                            }
-                        )}
-                            className="input input-bordered w-full" />
-                        {errors.password && <p className='text-red-600'>{errors.password.message}</p>}
-                        <label className="label"><span className="label-text">Fotget password</span> </label>
-                    </div>
-                    <input type="submit" className=' btn btn-primary  bg-gradient-to-r from-primary  to-secondary my-2 text-white w-full' value='Sign Up' />
                     <div>
-                        {singUpError && <p className='text-red-600'>{singUpError}</p>}
+                        <h1 className=" text-5xl font-bold text-blue-600">How it works
+                        </h1>
+                        <p className="py-6 text-2xl font-semibold">We began Tech.com back in 2020 to give used electronics a new life. Since then, we’ve saved thousands of gadgets from landfills, and paid our customers in the process.</p>
+                        <PrimaryButten>Get start</PrimaryButten>
                     </div>
-                </form>
-                <p className='font-bold text-center'>Already have an <Link to='/login' className='text-secondary'> account</Link></p>
-               
+                </div>
             </div>
+
         </div>
     );
 };
