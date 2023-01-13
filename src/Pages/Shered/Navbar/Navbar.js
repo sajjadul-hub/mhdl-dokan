876d3b73300df0ec0d939dbe0010c53a33c80022
sajjadul-hub <sajjadulislamsp@gmail.com>
2023-01-13 19:28:48 +0600

    MHDL-dokan

--- src/Pages/Shered/Navbar/Navbar.js
+++ src/Pages/Shered/Navbar/Navbar.js
@@ -40,8 +40,8 @@ const Navbar = () => {
             }
           </ul>
         </div>
-        <img style={{width:'60px',height:'60px'}} src={navicon} alt=''></img>
-        <Link to='/' className="btn btn-ghost normal-case text-xl" href='/'>Tech.com</Link>
+        <img style={{ width: '60px', height: '60px' }} src={navicon} alt=''></img>
+        <Link to='/' className="btn btn-ghost normal-case text-xl" href='/'>MHDL-Dokan</Link>
       </div>
       <div className="navbar-center hidden lg:flex">
         <ul className="menu menu-horizontal p-0">
@@ -51,8 +51,8 @@ const Navbar = () => {
         </ul>
       </div>
       <label tabIndex={2} htmlFor="dashboard-drawer" className="btn btn-ghost lg:hidden">
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-          </label>
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+      </label>
     </div>
   );
 };
